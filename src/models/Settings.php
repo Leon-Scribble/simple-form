b242c99701995df41c7122c22e6390d4b561996a
Leon-Scribble <137173728+Leon-Scribble@users.noreply.github.com>
2023-06-29 14:39:45 +0200

    - removed rules

--- src/models/Settings.php
+++ src/models/Settings.php
@@ -12,12 +12,6 @@ class Settings extends Model
 {
     public $receiver = 'example@example.com';
     public $subject = '[SimpleForm] Default';
-    public function defineRules(): array
-    {
-        return [
-            [['receiver', 'subject']],
-        ];
-    }
 
     protected function createSettingsModel(): ?Model
     {
